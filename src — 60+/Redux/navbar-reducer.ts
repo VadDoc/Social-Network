7624d71ab7d93b50a6b17dd4930133665bfca098
VadDoc <vadprudnikov@gmail.com>
2021-10-27 11:06:38 +0300

    Lesson 60: add socials logo to Profile

---
+++ src — 60+/Redux/navbar-reducer.ts
@@ -0,0 +1,22 @@
+import {NavigationType} from "../components/Navbar/Navbar";
+import {v1} from "uuid";
+import {ActionsType} from "./redux-store";
+
+export type NavBarType = {
+  navigation: Array<NavigationType>
+}
+
+const initialState = {
+  navigation: [
+    {id: v1(), pageName: 'Profile', link: '/profile'},
+    {id: v1(), pageName: 'Messages', link: '/dialogs'},
+    {id: v1(), pageName: 'Users', link: '/users'},
+    {id: v1(), pageName: 'News', link: '/news'},
+    {id: v1(), pageName: 'Music', link: '/music'},
+    {id: v1(), pageName: 'Settings', link: '/settings'},
+  ]
+}
+
+export const navbarReducer = (state: NavBarType = initialState, action: ActionsType) => {
+  return state
+}
